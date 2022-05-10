73185bf2a2fc77b30a788e181fa70046495de58c
JCNaranjoP <juan.naranjop@upb.edu.co>
2022-05-10 18:30:50 -0500

    Geolocalizacion

--- app/src/main/java/co/upb/sportownative/CalidadAire.kt
+++ app/src/main/java/co/upb/sportownative/CalidadAire.kt
@@ -1,8 +1,15 @@
 package co.upb.sportownative
 
+import android.Manifest
 import android.content.Context
+import android.content.pm.PackageManager
+import android.location.Location
 import android.os.Bundle
+import android.util.Half.toFloat
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
@@ -13,6 +20,8 @@ import com.google.android.gms.maps.model.MarkerOptions
 import kotlinx.android.synthetic.main.activity_home.*
 
 class CalidadAire : AppCompatActivity(), OnMapReadyCallback {
+
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,16 +41,36 @@ class CalidadAire : AppCompatActivity(), OnMapReadyCallback {
         val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as? SupportMapFragment
         mapFragment?.getMapAsync(this)
 
+
+
         //Recordar borrar datos al cerrar sesión
     }
     override fun onMapReady(googleMap: GoogleMap) {
-        val ubication = LatLng(6.243163, -75.602764)
-        googleMap.addMarker(
-            MarkerOptions()
-                .position(ubication)
-                .title("AQI: 20")
-        )
-        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(ubication, 15F))
+        //geo localizacion
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION), 181)
+
+        }
+        fusedLocationClient.lastLocation.addOnSuccessListener { location : Location ->
+            val lat = location.latitude
+            val lon = location.longitude
+            val ubication = LatLng(lat,lon)
+            googleMap.addMarker(
+                MarkerOptions()
+                    .position(ubication)
+                    .title("AQI: 20")
+            )
+            googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(ubication, 15F))
+        }
+
     }
     private fun setup(email: String){
 
